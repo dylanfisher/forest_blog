4fec3bd290556a62d27f102d7df64328b734015b
dylanfisher <hi@dylanfisher.com>
2017-02-20 01:45:28 -0500

    Start working on controller scaffold

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
-class ApplicationController < ActionController::Base
+# TODO: inherit from Forest::ApplicationController automatically
+class ApplicationController < Forest::ApplicationController
   protect_from_forgery with: :exception
 end
