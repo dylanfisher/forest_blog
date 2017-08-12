74af92b8c963fb3abcac48f509f024e51be3c453
dylanfisher <hi@dylanfisher.com>
2017-08-12 12:57:12 -0400

    More progress on image gallery block

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170810040457) do
+ActiveRecord::Schema.define(version: 20170812161938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
