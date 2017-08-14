616f43836ce9f80abfc2847f1c0e219940452409
dylanfisher <hi@dylanfisher.com>
2017-08-14 00:56:09 -0400

    Continue refactor

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170812161938) do
+ActiveRecord::Schema.define(version: 20170813173154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,29 +20,30 @@ ActiveRecord::Schema.define(version: 20170812161938) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "block_slots", id: :serial, force: :cascade do |t|
-    t.integer "block_id"
-    t.string "layout"
-    t.string "block_type", null: false
-    t.string "block_record_type"
-    t.integer "block_record_id"
-    t.integer "position", default: 0, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["block_id", "block_type"], name: "index_block_slots_on_block_id_and_block_type"
-    t.index ["block_record_type", "block_record_id"], name: "index_block_slots_on_block_record_type_and_block_record_id"
-    t.index ["block_type", "block_id"], name: "index_block_slots_on_block_type_and_block_id"
-    t.index ["layout"], name: "index_block_slots_on_layout"
-  end
-
-  create_table "block_types", id: :serial, force: :cascade do |t|
+  create_table "block_kinds", id: :serial, force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.string "category"
     t.boolean "active", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_block_types_on_name", unique: true
+    t.index ["name"], name: "index_block_kinds_on_name", unique: true
+  end
+
+  create_table "block_slots", id: :serial, force: :cascade do |t|
+    t.string "block_type"
+    t.integer "block_id"
+    t.integer "block_kind_id"
+    t.string "block_record_type"
+    t.integer "block_record_id"
+    t.string "layout"
+    t.integer "position", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["block_kind_id"], name: "index_block_slots_on_block_kind_id"
+    t.index ["block_record_type", "block_record_id"], name: "index_block_slots_on_block_record_type_and_block_record_id"
+    t.index ["block_type", "block_id"], name: "index_block_slots_on_block_type_and_block_id"
+    t.index ["layout"], name: "index_block_slots_on_layout"
   end
 
   create_table "image_blocks", id: :serial, force: :cascade do |t|
@@ -172,5 +173,6 @@ ActiveRecord::Schema.define(version: 20170812161938) do
     t.index ["slug"], name: "index_users_on_slug", unique: true
   end
 
+  add_foreign_key "block_slots", "block_kinds"
   add_foreign_key "image_gallery_block_images", "image_gallery_blocks"
 end
