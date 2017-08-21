2daa583b6a8f34f703987b52b4fe010c1d91bc78
dylanfisher <hi@dylanfisher.com>
2017-08-20 23:06:03 -0400

    Remove unused test routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,3 @@
 Rails.application.routes.draw do
-  scope :admin do
-    # resources :artists
-    # resources :artworks
-    # resources :exhibitions
-  end
-
-  # TODO: polymorphic path for versions to live in engine?
-  get 'exhibitions/:id/versions', to: 'exhibitions#versions', as: 'exhibition_versions'
-  get 'exhibitions/:id/versions/:version_id', to: 'exhibitions#version', as: 'exhibition_version'
-  get 'exhibitions/:id/versions/:version_id/restore', to: 'exhibitions#restore', as: 'restore_exhibition_version'
-
-  # Show
-  get 'artists/:id', to: 'artists#show', as: 'show_artist'
-  get 'artworks/:id', to: 'artworks#show', as: 'show_artwork'
-  get 'exhibitions/:id', to: 'exhibitions#show', as: 'show_exhibition'
-
   mount Forest::Engine, at: '/'
 end
